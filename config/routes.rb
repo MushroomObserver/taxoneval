3303664e6ad8864839e91b64cf9f2f5f312cc182
Nathan Wilson <nathan@collectivesource.com>
2013-12-15 08:26:19 -0500

    Added initial empty views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Taxoneval::Application.routes.draw do
+  get "report/show"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
