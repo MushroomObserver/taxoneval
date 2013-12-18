64126cf17bae210a03873bd56bfe1a155d0a434d
Nathan Wilson <nathan@collectivesource.com>
2013-12-18 07:53:46 -0500

    FactoryGirl() is no longer the same as FactoryGirl.create

--- spec/models/report_spec.rb
+++ spec/models/report_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe Report do
   it "has a valid factory" do
-    FactoryGirl(:report).should be_valid
+    FactoryGirl.create(:report).should be_valid
   end
   it "is invalid without a taxon" do
     FactoryGirl.build(:report, taxon: nil).should_not be_valid
